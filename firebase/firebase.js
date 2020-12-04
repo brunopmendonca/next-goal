5e663cf46cda23985a323420c6dc70ea3e11721e
Bruno César Piovezan Mendonça <bcm_1993@hotmail.com>
2020-12-04 15:22:04 -0300

    primeira atualização

---
+++ firebase/firebase.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyASeqTian2KWhnsNAn35f2-Z7vP5Jm5fV4",
+    authDomain: "next-goal-79fe3.firebaseapp.com",
+    projectId: "next-goal-79fe3",
+    storageBucket: "next-goal-79fe3.appspot.com",
+    databaseURL: "",
+    messagingSenderId: "977486983157",
+    appId: "1:977486983157:web:86d0469341ab6dc69bbabc",
+    measurementId: "G-TZJMP4MZBP"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
